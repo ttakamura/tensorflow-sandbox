308a48f713e3bba5c099fd6e4e848e08368c3a42
Tatsuya Takamura <takamura@tonchidot.com>
2016-05-23 17:23:02 +0900

    prepare conv2d

--- deep_mnist/main.py
+++ deep_mnist/main.py
@@ -1,13 +1,28 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
 sess  = tf.InteractiveSession()
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 x  = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
-W  = tf.Variable(tf.zeros([784, 10]))
-b  = tf.Variable(tf.zeros([10]))
+
+W  = weight_variable([784, 10])
+b  = bias_variable([10])
 
 sess.run(tf.initialize_all_variables())
 
