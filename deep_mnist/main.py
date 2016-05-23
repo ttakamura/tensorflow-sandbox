72d0887188fb2164e4de4c50ce21856c48b050b0
Tatsuya Takamura <takamura@tonchidot.com>
2016-05-23 17:10:03 +0900

    MNIST

---
+++ deep_mnist/main.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+sess  = tf.InteractiveSession()
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+x  = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+W  = tf.Variable(tf.zeros([784, 10]))
+b  = tf.Variable(tf.zeros([10]))
+
+sess.run(tf.initialize_all_variables())
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+# --- get numpy array ---
+#
+# y.eval(feed_dict={ x: batch[0], y_: batch[1] })
+#
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step    = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+for j in range(10):
+    for i in range(100):
+        batch = mnist.train.next_batch(50)
+        train_step.run(feed_dict={ x: batch[0], y_: batch[1] })
+    print("loop %d -----" % (j * 100))
+    print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
