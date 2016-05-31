2cc779218396142b9bc70d6818b4627624a5c88f
Tatsuya Takamura <takamura@tonchidot.com>
2016-05-31 19:03:20 +0900

    [from now] Tue May 31 19:03:20 JST 2016

--- tab_products/model.py
+++ tab_products/model.py
@@ -26,13 +26,12 @@ def conv2d(x, W):
 def max_pool_2x2(x):
   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-def conv_and_max_pool_layer(x, input_channel, output_channel):
+def conv_layer(x, input_channel, output_channel):
   W = weight_variable([5, 5, input_channel, output_channel])
   b = bias_variable([output_channel])
   z = conv2d(x, W) + b
   h = tf.nn.relu(batch_normalize(z))
-  h_pool = max_pool_2x2(h)
-  return W, b, h, h_pool
+  return W, b, h
 
 def fc_layer(x, input_dim, output_dim):
   W = weight_variable([input_dim, output_dim], wd=0.01)
@@ -41,19 +40,21 @@ def fc_layer(x, input_dim, output_dim):
   return W, b, h
 
 def small_model(x_image, width, height, input_channel, output_dim, dropout_ratio):
-  c1_channel = 20
+  c1_channel = 32
   c1_width   = width / 2
   c1_height  = height / 2
-  c2_channel = 40
+  c2_channel = 64
   c2_width   = c1_width / 2
   c2_height  = c1_height / 2
-  fc1_dim    = 30
+  fc1_dim    = 40
 
   with tf.variable_scope('conv1') as scope:
-    W_conv1, b_conv1, h_conv1, h_pool1 = conv_and_max_pool_layer(x_image, input_channel, c1_channel)
+    W_conv1, b_conv1, h_conv1 = conv_layer(x_image, input_channel, c1_channel)
+    h_pool1 = max_pool_2x2(h_conv1)
 
   with tf.variable_scope('conv2') as scope:
-    W_conv2, b_conv2, h_conv2, h_pool2 = conv_and_max_pool_layer(h_pool1, c1_channel, c2_channel)
+    W_conv2, b_conv2, h_conv2 = conv_layer(h_pool1, c1_channel, c2_channel)
+    h_pool2 = max_pool_2x2(h_conv2)
 
   h_pool2_dim  = int(c2_width * c2_height * c2_channel)
   h_pool2_flat = tf.reshape(h_pool2, [-1, h_pool2_dim])
