0b978aaee1dcaf081f763c0927cc04d6c7ac99ef
Tatsuya Takamura <takamura@tonchidot.com>
2016-05-31 11:52:00 +0900

    [from now] Tue May 31 11:51:59 JST 2016

--- tab_products/trainer.py
+++ tab_products/trainer.py
@@ -2,9 +2,12 @@ import tensorflow as tf
 
 def optimizer(logits, labels, learn_rate, global_step):
   with tf.variable_scope('optimizer') as scope:
-    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, labels, name='cross_entropy')
-    loss          = tf.reduce_mean(cross_entropy, name='loss')
-    train_opt     = tf.train.AdamOptimizer(learn_rate).minimize(loss, global_step=global_step, name='train_opt')
+    cross_entropy      = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, labels, name='cross_entropy')
+    cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy_mean')
+    tf.add_to_collection('losses', cross_entropy_mean)
+    loss               = tf.add_n(tf.get_collection('losses'), name='total_loss')
+    train_opt          = tf.train.AdamOptimizer(learn_rate).minimize(loss, global_step=global_step, name='train_opt')
+    tf.scalar_summary('cross_entropy', cross_entropy_mean)
     tf.scalar_summary('loss', loss)
   return train_opt
 
