965604e69e8ecffb7985dbfd69b2d75fca43826d
Tatsuya Takamura <takamura@tonchidot.com>
2016-06-02 16:27:16 +0900

    [from now] Thu Jun  2 16:27:16 JST 2016

--- tutorials/try_tfrecord_shuffle_csv.py
+++ tutorials/try_tfrecord_shuffle_csv.py
@@ -9,16 +9,25 @@ key, value = reader.read(filename_queue)
 col1, col2, col3, col4, col5 = tf.decode_csv(value, record_defaults=[[1], [1], [1], [1], [1]])
 features = tf.pack([col1, col2, col3, col4])
 
+batch_size = 10
+min_after_dequeue = 100
+capacity = min_after_dequeue + 3 * batch_size
+
+features_batch = tf.train.shuffle_batch([features],
+                                        batch_size=batch_size,
+                                        capacity=capacity,
+                                        min_after_dequeue=min_after_dequeue)
+
+y = features_batch * 10
+
 with tf.Session() as sess:
   # Start populating the filename queue.
   coord   = tf.train.Coordinator()
   threads = tf.train.start_queue_runners(coord=coord)
 
-  print("request")
   for i in range(100):
-    print(sess.run([key, value]))
-    print(sess.run([features, col5]))
-  print("done")
+    print("-------- %d ---------" % i)
+    print(sess.run(y))
 
   coord.request_stop()
   coord.join(threads)
